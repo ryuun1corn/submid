0d0b29ce981dd4e6bb01424501e694f7fe1f12af
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-04-29 19:49:08 +0700

    feat: styling and about us, create module

--- src/submid_frontend/src/App.tsx
+++ src/submid_frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import { ModeToggle } from '@/components/mode-toggle';
-import HomepageModule from './components/modules/HomepageModule';
+import { ChevronsLeft } from 'lucide-react';
+import { Link, Outlet } from 'react-router-dom';
+import { Button } from '@/components/ui/button';
 
 function App() {
   return (
     <main className="min-h-screen bg-gray-50 dark:bg-gray-950 flex flex-col items-center justify-center gap-5">
-      <div className="absolute top-10 right-10">
+      <div className="absolute top-10 flex flex-row items-center justify-between w-full px-10">
+        <Button asChild variant="outline">
+          <Link to={'/'}>
+            <ChevronsLeft className="h-4 w-4" />
+          </Link>
+        </Button>
         <ModeToggle />
       </div>
-      <HomepageModule />
+      <Outlet />
     </main>
   );
 }
---
+++ src/submid_frontend/src/components/modules/AboutModule/index.tsx
@@ -0,0 +1,18 @@
+const AboutModule = () => {
+  return (
+    <>
+      <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl">
+        POKoknyaPOK
+      </h1>
+      <p>About Us</p>
+
+      <p className="text-center tracking-wider leading-7">
+        Rifqisyandi Khairurrizal <br /> Raihan Akbar <br /> Kaindra Rizq Sachio{' '}
+        <br />
+        Yudayana Arif Prasojo
+      </p>
+    </>
+  );
+};
+
+export default AboutModule;
