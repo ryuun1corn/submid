a8c0f7c3812c12748d66fbaf7dda3bb5df71d065
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-04-29 19:49:36 +0700

    feat: styling and about us, create module

--- src/submid_frontend/src/components/modules/CreateFormModule/index.tsx
+++ src/submid_frontend/src/components/modules/CreateFormModule/index.tsx
@@ -49,57 +49,63 @@ const CreateFormModule = () => {
   }, [setPrincipal]);
 
   return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Call yourself</CardTitle>
-        <CardDescription>
-          Initiate a call to yourself in one click.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form
-          action="#"
-          onSubmit={handleSubmit}
-          className="flex flex-col gap-3"
-        >
-          <label htmlFor="name">Enter your name: &nbsp;</label>
-          <input
-            id="name"
-            name="name"
-            alt="Name"
-            type="text"
-            className="border-[2px] rounded-md p-2 dark:text-black"
-          />
-          <Button type="submit">Click me!</Button>
-          <section id="greeting">{greeting}</section>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <HoverCard>
-          <HoverCardTrigger asChild>
-            <Button variant="link">Show Principal</Button>
-          </HoverCardTrigger>
-          <HoverCardContent className="">
-            <div className="flex justify-between space-x-4">
-              <div className="space-y-1">
-                <h4 className="text-sm font-semibold">Your Principal:</h4>
-                <p className="text-sm">{principal}</p>
-              </div>
-            </div>
-          </HoverCardContent>
-        </HoverCard>
+    <>
+      <div className="absolute top-10 left-10"></div>
+      {!authenticated ? (
+        <div className="flex flex-col items-center gap-5">
+          <div>You are not authenticated yet</div>
 
-        {authenticated ? (
-          <Button variant="destructive" onClick={logout}>
-            Logout
-          </Button>
-        ) : (
           <Button variant="secondary" onClick={login}>
             Login
           </Button>
-        )}
-      </CardFooter>
-    </Card>
+        </div>
+      ) : (
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Call yourself</CardTitle>
+            <CardDescription>
+              Initiate a call to yourself in one click.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form
+              action="#"
+              onSubmit={handleSubmit}
+              className="flex flex-col gap-3"
+            >
+              <label htmlFor="name">Enter your name: &nbsp;</label>
+              <input
+                id="name"
+                name="name"
+                alt="Name"
+                type="text"
+                className="border-[2px] rounded-md p-2 dark:text-black"
+              />
+              <Button type="submit">Click me!</Button>
+              <section id="greeting">{greeting}</section>
+            </form>
+          </CardContent>
+          <CardFooter className="flex justify-between">
+            <HoverCard>
+              <HoverCardTrigger asChild>
+                <Button variant="link">Show Principal</Button>
+              </HoverCardTrigger>
+              <HoverCardContent className="">
+                <div className="flex justify-between space-x-4">
+                  <div className="space-y-1">
+                    <h4 className="text-sm font-semibold">Your Principal:</h4>
+                    <p className="text-sm">{principal}</p>
+                  </div>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
+            <Button variant="destructive" onClick={logout}>
+              Logout
+            </Button>
+          </CardFooter>
+        </Card>
+      )}
+    </>
   );
 };
 
--- src/submid_frontend/src/main.tsx
+++ src/submid_frontend/src/main.tsx
@@ -5,12 +5,29 @@ import './index.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import NotFoundModule from './components/modules/NotFoundModule/index.tsx';
+import CreateFormModule from './components/modules/CreateFormModule/index.tsx';
+import HomepageModule from './components/modules/HomepageModule/index.tsx';
+import AboutModule from './components/modules/AboutModule/index.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     errorElement: <NotFoundModule />,
+    children: [
+      {
+        path: '/',
+        element: <HomepageModule />,
+      },
+      {
+        path: '/create',
+        element: <CreateFormModule />,
+      },
+      {
+        path: '/about',
+        element: <AboutModule />,
+      },
+    ],
   },
 ]);
 
