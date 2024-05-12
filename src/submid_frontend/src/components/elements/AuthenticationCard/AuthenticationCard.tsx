82ce079a0cfc2c39677a1de1489d53e2d3f10ea2
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-05-12 07:42:27 +0700

    fix: remove suspense, show principal, and add validation message

--- src/submid_frontend/src/components/elements/AuthenticationCard/AuthenticationCard.tsx
+++ src/submid_frontend/src/components/elements/AuthenticationCard/AuthenticationCard.tsx
@@ -5,17 +5,11 @@ import {
   CardTitle,
   CardDescription,
   CardContent,
-  CardFooter,
 } from '@/components/ui/card';
-import { Suspense, useState } from 'react';
+import { useState } from 'react';
 import { useAuthContext } from '../../contexts/UseAuthContext';
 import { AuthenticationCardPropsInterface } from './interface';
 import { Input } from '@/components/ui/input';
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from '@/components/ui/hover-card';
 import { z } from 'zod';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -53,72 +47,53 @@ const AuthenticationCard: React.FC<AuthenticationCardPropsInterface> = ({
     setIsLoading(true);
     createProfile(name).then(() => {
       setIsLoading(false);
+      form.setValue('username', '');
     });
   }
 
-  const LoadComponent = () => {
-    if (profile === undefined) throw new Promise(() => {});
+  return profile === undefined ? (
+    <div>Loading...</div>
+  ) : !isAuthenticated ? (
+    <div className="flex flex-col items-center gap-5">
+      <div>You are not authenticated yet</div>
 
-    return !isAuthenticated ? (
-      <div className="flex flex-col items-center gap-5">
-        <div>You are not authenticated yet</div>
-
-        <Button variant="secondary" onClick={login}>
-          Login
-        </Button>
-      </div>
-    ) : profile === null ? (
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Create an account</CardTitle>
-          <CardDescription>
-            This account is bound to your Internet Identity
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit" disabled={isLoading}>
-                Submit
-              </Button>
-            </form>
-          </Form>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <HoverCard>
-            <HoverCardTrigger asChild>
-              <Button variant="link">See principal</Button>
-            </HoverCardTrigger>
-            <HoverCardContent className="font-mono">
-              {authClient?.getIdentity().getPrincipal().toString()}
-            </HoverCardContent>
-          </HoverCard>
-        </CardFooter>
-      </Card>
-    ) : (
-      children
-    );
-  };
-
-  return (
-    <>
-      <Suspense fallback={<div>loading...</div>}>
-        <LoadComponent />
-      </Suspense>
-    </>
+      <Button variant="secondary" onClick={login}>
+        Login
+      </Button>
+    </div>
+  ) : profile === null ? (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Create an account</CardTitle>
+        <CardDescription>
+          This account is bound to your Internet Identity
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isLoading}>
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  ) : (
+    children
   );
 };
 
