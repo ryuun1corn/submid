dbc3b41ec493580eddba68a3fb50e7d54e4d6208
ryuun1corn <yuda.arif.prasojo@gmail.com>
2024-05-13 18:22:00 +0700

    refactor: change navbar to use <nav> and change home icon

--- src/submid_frontend/src/App.tsx
+++ src/submid_frontend/src/App.tsx
@@ -1,19 +1,19 @@
 import { ModeToggle } from '@/components/mode-toggle';
-import { ChevronsLeft } from 'lucide-react';
+import { HomeIcon } from 'lucide-react';
 import { Link, Outlet } from 'react-router-dom';
 import { Button } from '@/components/ui/button';
 
 function App() {
   return (
     <main className="min-h-screen bg-gray-50 dark:bg-gray-950 flex flex-col items-center justify-center gap-5">
-      <div className="absolute top-10 flex flex-row items-center justify-between w-full px-10">
-        <Button asChild variant="outline">
+      <nav className="fixed top-0 flex flex-row items-center justify-between w-full p-10">
+        <Button asChild variant="ghost">
           <Link to={'/'}>
-            <ChevronsLeft className="h-4 w-4" />
+            <HomeIcon className="h-6 w-6" />
           </Link>
         </Button>
         <ModeToggle />
-      </div>
+      </nav>
       <Outlet />
     </main>
   );
